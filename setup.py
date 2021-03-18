4995f365e31d817a0d8a66a09603ea156bf8d984
charludo <github@charlotteharludo.com>
2021-03-18 16:56:21 +0100

    Installed barely as editable module

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(name='barely',
+                 version='0.1',
+                 description='barely Development',
+                 url='#',
+                 author='charlotte',
+                 author_email='',
+                 packages=setuptools.find_packages(),
+                 zip_safe=False)
