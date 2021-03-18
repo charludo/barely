4c5dd9c6e22262c886628131c9d965d92b1821f2
charludo <github@charlotteharludo.com>
2021-03-18 11:08:09 +0100

    Finisehd plugin classes, still no testing

--- barely/plugins/PluginManager.py
+++ barely/plugins/PluginManager.py
@@ -24,7 +24,7 @@ class PluginBase:
         pass
 
     def register(self):
-        return "Base", [], -1
+        return "Base", -1, []
 
     def action(self, *args, **kwargs):
         if "item" in kwargs:
@@ -38,10 +38,13 @@ class PluginManager:
 
     def __init__(self):
         self.plugins_content = self.discover_plugins([config["PLUGIN_PATHS"]["SYS"]["CONTENT"], [config["PLUGIN_PATHS"]["USER"]["CONTENT"]]])
+        self.plugins_backup = self.discover_plugins([config["PLUGIN_PATHS"]["SYS"]["BACKUP"], [config["PLUGIN_PATHS"]["USER"]["BACKUP"]]], type_content=False)
+        self.plugins_publication = self.discover_plugins([config["PLUGIN_PATHS"]["SYS"]["PUBLICATION"],
+                                                         [config["PLUGIN_PATHS"]["USER"]["PUBLICATION"]]], type_content=False)
 
-    def discover_plugins(self, paths):
+    def discover_plugins(self, paths, type_content=True):
         """ checks the path for plugin files, then imports them """
-        found_plugins = {}
+        found_plugins = {} if type_content else []
         for path in paths:
             for (_, module_name, _) in iter_modules([path]):
 
@@ -50,14 +53,21 @@ class PluginManager:
                     attribute = getattr(module, attribute_name)
 
                     if issubclass(attribute, PluginBase):
-                        name, registered_for, priority = attribute.register()
-                        if name not in config["DISABLED_PLUGINS"]:
+                        if type_content:
+                            name, priority, registered_for = attribute.register()
                             for extension in registered_for:
                                 found_plugins.setdefault(extension, []).append((attribute, priority))
+                        else:
+                            name, priority = attribute.register()
+                            found_plugins.append((attribute, priority))
 
-        for ext, registered in found_plugins.items():
-            registered.sort(key=lambda t: t[1])
-            found_plugins[ext] = [r[0] for r in registered]
+        if type_content:
+            for ext, registered in found_plugins.items():
+                registered.sort(key=lambda t: t[1])
+                found_plugins[ext] = [r[0] for r in registered]
+        else:
+            found_plugins.sort(key=lambda t: t[1])
+            found_plugins = [r[0] for r in found_plugins]
 
         return found_plugins
 
@@ -72,7 +82,9 @@ class PluginManager:
         return item_list
 
     def hook_backup(self):
-        pass
+        for plugin in self.plugins_backup:
+            plugin.action()
 
     def hook_publication(self):
-        pass
+        for plugin in self.plugins_publication:
+            plugin.action()
