1e768e07828d732df2ae0abf7eb6cdbf3e55a1b4
charludo <github@charlotteharludo.com>
2021-04-03 20:19:06 +0200

    Created git Plugin

--- README.md
+++ README.md
@@ -2,4 +2,3 @@ WANTED PLUGINS:
 - contact forms (def: basic data, def: input element generation, def: form groups, call itself or inp. elem.)
 - ftp upload
 - auto git commit (blueprint must not contain .git!!)
-- local backup
---
+++ barely/plugins/backup/git/git.py
@@ -0,0 +1,38 @@
+"""
+push to git remote rep automatically
+"""
+
+import os
+from git import Repo
+from datetime import datetime
+from barely.plugins import PluginBase
+
+
+class LocalBackup(PluginBase):
+    # add changes / commit them / push them to origin
+
+    def __init__(self):
+        super().__init__()
+        try:
+            standard_config = {
+                "PRIORITY": 40,
+                "MESSAGE": "barely auto commit",
+                "REMOTE_NAME": "origin"
+            }
+            self.plugin_config = standard_config | self.config["GIT"]
+        except KeyError:
+            self.plugin_config = {"PRIORITY": -1}
+
+    def register(self):
+        return "git", self.plugin_config["PRIORITY"]
+
+    def action(self, *args, **kwargs):
+        try:
+            repo = Repo(os.path.join(self.config["ROOT"]["DEV"], ".git"))
+            repo.git.add(update=True)
+            repo.index.commit(datetime.now().strftime("%Y-%m-%d--%H-%M-%S") + " " + self.plugin_config["MESSAGE"])
+            origin = repo.remote(name=self.plugin_config["REMOTE_NAME"])
+            origin.push()
+            print(f"barely :: successfully pushed to {self.plugin_config['REMOTE_NAME']}")
+        except Exception:
+            print(f"barely :: an error occurred while pushing to {self.plugin_config['REMOTE_NAME']}")
