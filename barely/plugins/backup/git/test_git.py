ae70bb6d89914b06ba3c0e3d7ab345eeb35f4d05
charludo <github@charlotteharludo.com>
2021-05-03 10:51:19 +0200

    Testing system for plugins tested

---
+++ barely/plugins/backup/git/test_git.py
@@ -0,0 +1,14 @@
+import unittest
+from .git import Git
+
+
+class TestGit(unittest.TestCase):
+
+    def test___init__(self):
+        pass
+
+    def test_register(self):
+        pass
+
+    def test_action(self):
+        pass
