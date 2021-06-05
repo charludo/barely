6b4094e9c5492bd675b9fe89f2c6b1df34eca85d
charludo <github@charlotteharludo.com>
2021-06-05 19:05:19 +0200

    created (empty) test file for SFTP

---
+++ barely/plugins/publication/sftp/test_sftp.py
@@ -0,0 +1,14 @@
+import unittest
+from barely.plugins.publication.sftp.sftp import SFTP
+
+
+class TestGit(unittest.TestCase):
+
+    def test___init__(self):
+        pass
+
+    def test_register(self):
+        pass
+
+    def test_action(self):
+        pass
