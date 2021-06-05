aba3b6bc90ec9554ab6e972a039230b651ccd69e
charludo <github@charlotteharludo.com>
2021-06-05 21:20:40 +0200

    SFTP Plugin tested

--- barely/plugins/publication/sftp/test_sftp.py
+++ barely/plugins/publication/sftp/test_sftp.py
@@ -1,14 +1,62 @@
 import unittest
+from mock import patch
 from barely.plugins.publication.sftp.sftp import SFTP
 
 
-class TestGit(unittest.TestCase):
+class TestSFTP(unittest.TestCase):
 
     def test___init__(self):
-        pass
+        sftp = SFTP()
+        self.assertDictEqual({"PRIORITY": -1}, sftp.plugin_config)
+
+        sftp.config["SFTP"] = {"PRIORITY": 2}
+        sftp.__init__()
+
+        golden = {
+            "PRIORITY": 2,
+            "HOSTNAME": "",
+            "USER": "",
+            "PASSWORD": "",
+            "KEY": "",
+            "ROOT": ""
+        }
+
+        self.assertDictEqual(golden, sftp.plugin_config)
+
+        # reset
+        del sftp.config["SFTP"]
+        sftp.__init__()
 
     def test_register(self):
-        pass
+        sftp = SFTP()
+        name, prio = sftp.register()
 
-    def test_action(self):
-        pass
+        self.assertEqual(name, "sftp")
+        self.assertEqual(prio, -1)
+
+    @patch("barely.plugins.publication.sftp.sftp.pysftp.Connection")
+    def test_action(self, Connection):
+        sftp = SFTP()
+        sftp.plugin_config = {
+            "PRIORITY": 2,
+            "HOSTNAME": "",
+            "USER": "",
+            "PASSWORD": "",
+            "KEY": "",
+            "ROOT": ""
+        }
+        sftp.action()
+        Connection.assert_called_with("", username="", password="")
+
+        Connection.reset_mock()
+
+        sftp.plugin_config = {
+            "PRIORITY": 2,
+            "HOSTNAME": "",
+            "USER": "",
+            "PASSWORD": "",
+            "KEY": "key",
+            "ROOT": ""
+        }
+        sftp.action()
+        Connection.assert_called_with("", username="", private_key="key", private_key_pass=True)
