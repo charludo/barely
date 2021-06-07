359112b2f452469264083564ca9bd5d17b51b6cf
charludo <github@charlotteharludo.com>
2021-06-07 10:02:20 +0200

    Forms Plugin tested

--- barely/plugins/content/Forms/forms.py
+++ barely/plugins/content/Forms/forms.py
@@ -12,6 +12,31 @@ from barely.plugins import PluginBase
 class Forms(PluginBase):
     # generate an arbitrary amount of forms for any page, specified through yaml
 
+    form_defaults = {
+        "classes": "",
+        "action": ""
+    }
+
+    group_defaults = {
+        "classes": "",
+        "legend": ""
+    }
+
+    field_defaults = {
+        "required": False,
+        "label": False,
+        "type": "",
+        "options": {},
+        "default": "",
+        "multiple": False,
+        "classes": "",
+        "placeholder": "",
+        "value": "",
+        "text": "",
+        "rows": "",
+        "cols": ""
+    }
+
     def __init__(self):
         super().__init__()
         standard_config = {
@@ -23,7 +48,7 @@ class Forms(PluginBase):
             self.plugin_config = standard_config
 
     def register(self):
-        return "Forms", self.plugin_config["PRIORITY"], self.config["PAGE_EXT"]
+        return "Forms", self.plugin_config["PRIORITY"], [self.config["PAGE_EXT"]]
 
     def action(self, *args, **kwargs):
         if "item" in kwargs:
@@ -34,17 +59,20 @@ class Forms(PluginBase):
                 yield item
                 return
 
-            for key, value in form_list:
-                item["form-" + key] = "\n".join(self.generate_form(key, value))
+            for key, value in form_list.items():
+                # try:
+                item["form-" + key] = "\n".join(self.generate_form(key, **value))
+                # except Exception:
+                #    pass
             yield item
 
     def generate_form(self, form_name, **form_config):
         form_config = self.form_defaults | form_config
         yield f"<form id=\"form-{form_name}\" class=\"{form_config['classes']}\" action=\"{form_config['action']}\">"
-        for field, settings in form_config:
+        for field, settings in form_config.items():
             if field in ["action", "classes"]:
                 continue
-            yield from self.render_fields(form_name, field, settings)
+            yield from self.render_fields(form_name, field, **settings)
         yield "</form>"
 
     def render_fields(self, form_name, field_name, **field_config):
@@ -57,10 +85,10 @@ class Forms(PluginBase):
             if field_config["legend"]:
                 yield f"<legend>{field_config['legend']}</legend>"
 
-            for field, settings in field_config:
+            for field, settings in field_config.items():
                 if field in ["legend", "classes"]:
                     continue
-                yield from self.render_fields(form_name, field, settings)
+                yield from self.render_fields(form_name, field, **settings)
 
             yield "</fieldset>"
 
@@ -71,21 +99,29 @@ class Forms(PluginBase):
                 yield f"<label for=\"{id}\">{field_config['label']}</label>"
 
             if field_config["type"] == "radio":
-                for value, option in field_config["options"]:
+                for value, option in field_config["options"].items():
                     checked = "checked" if value == field_config["default"] else ""
-                    radio_id = id + "-" + value
+                    radio_id = f"{id}-{value}"
                     yield f"<input type=\"radio\" id=\"{radio_id}\" value=\"{value}\" name=\"{field_name}\" {checked}>"
-                    yield f"<label for=\"{radio_id}\">option</label>"
+                    yield f"<label for=\"{radio_id}\">{option}</label>"
 
             elif field_config["type"] == "select":
                 multiple = "multiple" if field_config["multiple"] else ""
                 yield f"<select id=\"{id}\" class=\"{field_config['classes']}\" name=\"{field_name}\" {multiple} {required}>"
 
-                for value, option in field_config["options"]:
+                for value, option in field_config["options"].items():
                     selected = "selected" if value == field_config["default"] else ""
                     yield f"<option value=\"{value}\" {selected}>{option}</option>"
                 yield "</select>"
+
+            elif field_config["type"] == "button":
+                yield (f"<button type=\"{field_config['action']}\" id=\"{id}\" class=\"{field_config['classes']}\" "
+                       f"name=\"{field_name}\">{field_config['value']}</button>")
+
+            elif field_config["type"] == "textarea":
+                yield (f"<textarea id=\"{id}\" class=\"{field_config['classes']}\" rows=\"{field_config['rows']}\" cols=\"{field_config['cols']}\" "
+                       f"name=\"{field_name}\" placeholder=\"{field_config['placeholder']}\" {required}>{field_config['value']}</textarea>")
             else:
-                yield f"<input type=\"{field_config['type']} "
-                + f"id=\"{id}\" class=\"{field_config['classes']}\" "
-                + f"name=\"{field_name}\" value=\"{field_config['default']}\" placeholder=\"{field_config['placeholder']}\" {required}>"
+                yield (f"<input type=\"{field_config['type']}\" "
+                       f"id=\"{id}\" class=\"{field_config['classes']}\" "
+                       f"name=\"{field_name}\" value=\"{field_config['value']}\" placeholder=\"{field_config['placeholder']}\" {required}>")
---
+++ barely/plugins/content/Forms/test_forms.py
@@ -0,0 +1,125 @@
+import re
+import unittest
+from barely.plugins.content.Forms.forms import Forms
+
+
+class TestForms(unittest.TestCase):
+
+    def test___init__(self):
+        f = Forms()
+        self.assertDictEqual({"PRIORITY": 5}, f.plugin_config)
+
+        golden = {
+            "PRIORITY": 2
+        }
+        f.config["FORMS"] = {"PRIORITY": 2}
+        f.__init__()
+
+        self.assertDictEqual(golden, f.plugin_config)
+
+        # reset
+        del f.config["FORMS"]
+        f.__init__()
+
+    def test_register(self):
+        f = Forms()
+        name, prio, ext = f.register()
+
+        self.assertEqual(name, "Forms")
+        self.assertEqual(prio, 5)
+        self.assertEqual(ext, ["md"])
+
+    def test_action(self):
+        item = {
+            "meta": {
+                "forms": {
+                    "sample": {
+                        "action": "test.php",
+                        "classes": "large",
+                        "group-basic": {
+                            "legend": "Basic Data",
+                            "name": {
+                                "type": "text",
+                                "required": True,
+                                "placeholder": "Name"
+                            },
+                            "mmail": {
+                                "type": "email",
+                                "required": True,
+                                "placeholder": "E-Mail"
+                            }
+                        },
+                        "topic": {
+                            "type": "select",
+                            "multiple": False,
+                            "default": "standard",
+                            "options": {
+                                "n1": "something",
+                                "n2": "standard",
+                                "n3": "something else"
+                            }
+                        },
+                        "group-radios": {
+                            "rating": {
+                                "type": "radio",
+                                "label": "Please rate us!",
+                                "options": {
+                                    1: "1",
+                                    2: "2",
+                                    3: "3"
+                                }
+                            }
+                        },
+                        "message": {
+                            "type": "textarea",
+                            "required": True,
+                            "classes": "extra",
+                            "value": "Pre-filled"
+                        },
+                        "grpd": {
+                            "type": "checkbox",
+                            "value": "agreed",
+                            "label": "Agree to GDPR",
+                            "name": "gdpr-checkbox"
+                        },
+                        "send": {
+                            "type": "button",
+                            "value": "Send Now",
+                            "action": "submit"
+                        }
+                    }
+                }
+            }
+        }
+
+        golden_form = """
+        <form id="form-sample" class="large" action="test.php">
+        <fieldset id="form-sample-group-basic" class="">
+        <legend>Basic Data</legend>
+        <input type="text" id="form-sample-name" class="" name="name" value="" placeholder="Name" required>
+        <input type="email" id="form-sample-mmail" class="" name="mmail" value="" placeholder="E-Mail" required>
+        </fieldset>
+        <select id="form-sample-topic" class="" name="topic"  >
+        <option value="n1" >something</option>
+        <option value="n2" >standard</option>
+        <option value="n3" >something else</option>
+        </select>
+        <fieldset id="form-sample-group-radios" class="">
+        <label for="form-sample-rating">Please rate us!</label>
+        <input type="radio" id="form-sample-rating-1" value="1" name="rating" >
+        <label for="form-sample-rating-1">1</label>
+        <input type="radio" id="form-sample-rating-2" value="2" name="rating" >
+        <label for="form-sample-rating-2">2</label>
+        <input type="radio" id="form-sample-rating-3" value="3" name="rating" >
+        <label for="form-sample-rating-3">3</label>
+        </fieldset>
+        <textarea id="form-sample-message" class="extra" rows="" cols="" name="message" placeholder="" required>Pre-filled</textarea>
+        <label for="form-sample-grpd">Agree to GDPR</label>
+        <input type="checkbox" id="form-sample-grpd" class="" name="grpd" value="agreed" placeholder="" >
+        <button type="submit" id="form-sample-send" class="" name="send">Send Now</button>
+        </form>
+        """
+
+        f = Forms()
+        result = list(f.action(item=item))[0]
+        self.assertEqual(re.sub(r'[\s+]', '', golden_form), re.sub(r'[\s+]', '', result["form-sample"]))
