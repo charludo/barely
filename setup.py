78f342000ab6de312ddae0f4a29ca8dd2dbd8504
charludo <github@charlotteharludo.com>
2021-06-15 12:21:56 +0200

    added requirements to setup.py, removed requirements.txt

--- setup.py
+++ setup.py
@@ -9,5 +9,21 @@ setuptools.setup(name='barely',
                  author_email='',
                  packages=setuptools.find_packages(),
                  zip_safe=False,
-                 entry_points={"console_scripts": ["barely = barely.cli:run"]}
+                 entry_points={"console_scripts": ["barely = barely.cli:run"]},
+                 install_requires=[
+                    "click>=8.0.0",
+                    "mock>=4.0.0",
+                    "pyyaml>=5.3.0",
+                    "watchdog>=2.0.0",
+                    "pillow>=8.0.0",
+                    "GitPython>=3.0.0",
+                    "pygments>=2.5.0",
+                    "libsass>=0.21.0",
+                    "pysftp>=0.2.5",
+                    "livereload>=2.5.0",
+                    "binaryornot>=0.4.0",
+                    "jinja2>=3.0.0",
+                    "mistune==2.0.0rc1",
+                    "calmjs>=3.3.0"
+                 ]
                  )
