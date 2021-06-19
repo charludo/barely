c84b4b5138c9a236c8a040a0aa9c2599843c5df6
charludo <github@charlotteharludo.com>
2021-06-19 20:32:54 +0200

    making barely production ready!

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@ import setuptools
 
 
 setuptools.setup(name='barely',
-                 version='1.0.0',
+                 packages=['barely'],
+                 version='0.9.0',
                  description='barely is a lightweight, but highly extensible static site generator written in pure python.',
+                 keyword=['static site generator', 'jinja2', 'markdown', 'web development'],
                  url='https://github.com/charludo/barely',
+                 download_url='https://github.com/charludo/barely/archive/v_090.tar.gz',
                  author='Charlotte Hartmann Paludo',
                  author_email='contact@charlotteharludo.com',
+                 license='GPL-3.0',
                  packages=setuptools.find_packages(),
                  zip_safe=False,
                  entry_points={"console_scripts": ["barely = barely.cli:run"]},
@@ -25,5 +29,18 @@ setuptools.setup(name='barely',
                     "jinja2>=3.0.0",
                     "mistune==2.0.0rc1",
                     "calmjs>=3.3.0"
-                 ]
+                 ],
+                 classifiers=[
+                    'Development Status :: 4 - Beta',
+                    'Intended Audience :: Developers',
+                    'Topic :: Text Processing :: Markup :: HTML',
+                    'Topic :: Text Processing :: General',
+                    'Topic :: Software Development :: Build Tools',
+                    'Topic :: Software Development',
+                    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+                    'Natural Language :: English',
+                    'Programming Language :: Python :: 3.9',
+                    'Environment :: Console',
+                    'Operating System :: OS Independent'
+                  ],
                  )
