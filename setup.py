649c6b84fbadb2572b843e64d88d73a9bac93ded
charludo <github@charlotteharludo.com>
2021-06-19 21:05:04 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 
 setuptools.setup(name='barely',
-                 version='0.9.1',
+                 version='0.9.2',
                  description='barely is a lightweight, but highly extensible static site generator written in pure python.',
                  keyword=['static site generator', 'jinja2', 'markdown', 'web development'],
                  url='https://github.com/charludo/barely',
