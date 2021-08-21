c67eb31fd17ff87ddcc4f194f77ee43c2e37f647
charludo <github@charlotteharludo.com>
2021-08-21 10:03:06 +0200

    implemented handling of known (page specific/global + seo specific) tags

--- barely/plugins/content/AutoSEO/autoseo.py
+++ barely/plugins/content/AutoSEO/autoseo.py
@@ -22,22 +22,6 @@ class AutoSEO(PluginBase):
         except KeyError:
             self.plugin_config = standard_config
 
-        try:
-            with open(os.path.join(self.config["ROOT"]["DEV"], "metadata.yaml")) as file:
-                meta_raw = file.read()
-            meta = yaml.safe_load(meta_raw)
-            meta = {} if meta is None else meta
-        except FileNotFoundError:
-            meta = {}
-
-        self.meta = {
-            "title": "",
-            "keywords": "",
-            "description": "",
-            "site_name": "",
-            "site_url": ""
-        } | meta
-
     def register(self):
         return "AutoSEO", self.plugin_config["PRIORITY"], ["yaml", self.config["PAGE_EXT"]]
 
@@ -50,38 +34,53 @@ class AutoSEO(PluginBase):
             except KeyError:
                 page_seo = {}
 
-            def get_most_specific(tag):
-                if tag in page_seo:
-                    return page_seo[tag]
-                elif tag in item["meta"]:
-                    return item["meta"][tag]
-                elif tag in self.meta:
-                    return self.meta[tag]
-                return None
+            # Page-specific metadata; already merged with global metadata in ProcessingPipeline!
+            def get_page(tag, rebrand=None):
+                rebrand = rebrand if rebrand else tag
+                if tag in item["meta"]:
+                    return {rebrand: item["meta"][tag]}
+                return {}
 
-            def get_global(tag):
-                if tag in self.meta:
-                    return {tag: self.meta[tag]}
+            # Page & SEO specific metadata, can differ from page-specific one
+            def get_seo(tag, rebrand=None):
+                rebrand = rebrand if rebrand else tag
+                if tag in page_seo:
+                    return {rebrand: page_seo[tag]}
                 return {}
 
             # usw!! zusammen bauen mit: seo |= get_global(title) | get_page(title) | get_seo(title)
 
+            # 0: page-SEO specific
             # 1: page-specfic
-            # 2: global meta
             # 3: generate / guess
 
+            seo = {}
+
             # title
+            seo |= get_page("title")
             # description
+            seo |= get_page("description")
             # robots
 
             # og:title
+            seo |= get_page("title", "og:title") | get_seo("title", "og:title")
             # og:description
+            seo |= get_page("description", "og:description") | get_seo("description", "og:description")
             # og:image
+            seo |= get_page("title_image", "og:image") | get_seo("title_image", "og:image")
             # og:url
+            seo |= get_page("page_url", "og:url") | get_seo("page_url", "og:url")
             # og:site_name
+            seo |= get_page("title", "og:site_name") | get_page("site_name", "og:site_name") | get_seo("site_name", "og:site_name")
 
             # twitter:image:alt
+            seo |= get_page("description", "twitter:image:alt") | get_seo("description", "twitter:image:alt") | get_page("title_image_alt", "twitter:image:alt") | get_seo("title_image_alt", "twitter:image:alt")
             # twitter:card
+            seo |= {"twitter_card": "summary_card_large"} | get_page("twitter_card", "twitter:card") | get_seo("twitter_card", "twitter:card")
+            # twitter:site
+            seo |= get_page("twitter_site", "twitter:site") | get_seo("twitter_site", "twitter:site")
+            # twitter:creator
+            seo |= get_page("twitter_creator", "twitter:creator") | get_seo("twitter_creator", "twitter:creator")
 
             yield item
 
